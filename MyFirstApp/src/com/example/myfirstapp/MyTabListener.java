48c0fcb94c36ffebd19fcd98301c095cf10bf407
Gabriel Muñumel <gabrielmunumel@gmail.com>
2014-06-11 15:36:00 -0430

    Adding Tabs functionality. Still has errors.

---
+++ MyFirstApp/src/com/example/myfirstapp/MyTabListener.java
@@ -0,0 +1,31 @@
+package com.example.myfirstapp;
+
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+
+public class MyTabListener implements TabListener {
+	Fragment fragment;
+	
+	public MyTabListener(Fragment fragment) {
+		this.fragment = fragment;
+	}
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		ft.replace(R.id.fragment_container, fragment);
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		ft.remove(fragment);
+	}
+
+}
